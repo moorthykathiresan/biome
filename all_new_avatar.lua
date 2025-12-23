32d24b92e2770392d2a4410658eb43e4b293487c
moorthykathiresan <71226212+moorthykathiresan@users.noreply.github.com>
2025-12-23 12:09:11 -0500

    Add new epic narrative and agenda function

--- all_new_avatar.lua
+++ all_new_avatar.lua
@@ -1,17 +1,4 @@
 -- VetrI VeL MurugesaN ThunaI - GOD is Love and Love is GOD.
-function abilities()
-    return "One of my abilities is that I can ride 10 chariots at a time Naturally!"
-end
-
-function name()
-    return "One of my Abilities is one of my Name"
-end
-
-print('What are your Abilities?')
-print(abilities())
-print('What is your Name?')
-print(name())
-
 new_epic = [[
 Do you know, one of your own sons is going to compete with you, misjudging you
 driven by mother nature making an epic YANAM creating two more new Kingdoms? 
@@ -23,4 +10,19 @@ GIVEN to him and the remaining will be with you and your other sons.
 Greater Good Empire is on the way!
 ]]
 
-print(epic)
+function abilities()
+    return "One of my abilities is that I can ride 10 chariots at a time Naturally!"
+end
+
+function name()
+    return "One of my Abilities is one of my Name"
+end
+
+function agenda()
+    return new_epic; 
+end
+print('What are your Abilities?')
+print(abilities())
+print('What is your Name?')
+print(name())
+print(agenda())
