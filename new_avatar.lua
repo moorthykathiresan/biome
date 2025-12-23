c4736ff13dd153279f1f9093b6e0cb208c4b300a
moorthykathiresan <71226212+moorthykathiresan@users.noreply.github.com>
2025-12-23 13:49:36 +0000

    Added back the new_avatar

---
+++ new_avatar.lua
@@ -0,0 +1,16 @@
+function abilities()
+    return "Can ride 10 chariots at a time easily!"
+end
+
+function name()
+    return "Ability is one of my name"
+end
+
+print('What are your Abilities?')
+print(abilities())
+print('What is your Name?')
+print(name())
+local epic = [[
+Do you know your one of your own son is going to compete with you driven by nature
+making an epic YANAM creating two new kingdoms. Great Empire is on the way!]]
+print(epic)
