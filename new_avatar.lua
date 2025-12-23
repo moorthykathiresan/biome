0d2ce1ff91bd6ddb4a78224007f3cf21ea634d92
moorthykathiresan <71226212+moorthykathiresan@users.noreply.github.com>
2025-12-23 08:54:35 -0500

    Revise epic narrative for clarity and detail
    Updated the epic narrative to clarify the competition and kingdom distribution.

--- new_avatar.lua
+++ new_avatar.lua
@@ -11,6 +11,8 @@ print(abilities())
 print('What is your Name?')
 print(name())
 local epic = [[
-Do you know, one of your own son is going to compete with you driven by nature
-making an epic YANAM creating two new kingdoms, one for him and the remaining is yours. Great Empire is on the way!]]
+Do you know, one of your own son is going to compete with you driven by nature making
+an epic YANAM creating two more new kingdoms, one GIVEN to him and the remaining is for you
+and your other sons.
+Great Empire is on the way!]]
 print(epic)
