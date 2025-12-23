bf06d2251b94a15d8efdb46ad845422fdad0747e
moorthykathiresan <71226212+moorthykathiresan@users.noreply.github.com>
2025-12-23 12:54:31 -0500

    Fix spelling and improve narrative in all_new_avatar.lua
    Corrected spelling errors and improved phrasing in the epic narrative. R

--- all_new_avatar.lua
+++ all_new_avatar.lua
@@ -3,14 +3,19 @@ new_epic = [[
 Do you know, one of your own sons is going to compete with you, misjudging you
 driven by mother nature making an epic YANAM creating two more new Kingdoms? 
 He will compete with you naturally, unintentionally with innocense; paving way of helping
-good people in need on the way until your thrid son reveals the truth and reality to him.
+good people in need on the way until your third son reveals the truth and reality to him.
 You and everyone who knows this and involved in this will be distracted by MAYA (illusion) 
 RECURSIVELY in a LOOP. Every thing happening on the way will help him to master his skills
-such a way that he will be the King that everyone want to cherish and have good life.
+such a way that he will be the King that everyone want to bless and cherish heartfully and
+will have good life with. BOON!
 MAYA will guide your third son to the reality. With Two new Kingdoms, one of them will be
 GIVEN to him and the remaining will be with you and your other sons.
 Greater Good Empire is on the way! Note: This epic writting is itself distracted a bit by MAYA! :)
 ]]
+
+function agenda()
+    return new_epic; 
+end
 
 function abilities()
     return "One of my abilities is that I can ride 10 chariots at a time Naturally!"
@@ -20,9 +25,6 @@ function name()
     return "One of my Abilities is one of my Name"
 end
 
-function agenda()
-    return new_epic; 
-end
 print('What are your Abilities?')
 print(abilities())
 print('What is your Name?')
