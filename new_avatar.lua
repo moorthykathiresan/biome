4ceb07c1ba39f2659297b0609fdacd0bb25b3718
moorthykathiresan <71226212+moorthykathiresan@users.noreply.github.com>
2025-12-23 08:51:15 -0500

    Fix typo and improve epic message in new_avatar.lua

--- new_avatar.lua
+++ new_avatar.lua
@@ -11,6 +11,6 @@ print(abilities())
 print('What is your Name?')
 print(name())
 local epic = [[
-Do you know your one of your own son is going to compete with you driven by nature
-making an epic YANAM creating two new kingdoms. Great Empire is on the way!]]
+Do you know, one of your own son is going to compete with you driven by nature
+making an epic YANAM creating two new kingdoms, one for him and the remaining is yours. Great Empire is on the way!]]
 print(epic)
