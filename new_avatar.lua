6374fb1bf903e927479e9f0951c33f39f69f9473
moorthykathiresan <71226212+moorthykathiresan@users.noreply.github.com>
2025-12-23 09:30:34 -0500

    Fix typos and improve text clarity in new_avatar.lua

--- new_avatar.lua
+++ new_avatar.lua
@@ -1,10 +1,10 @@
 -- Vetri Vel Murugesan Thunai
 function abilities()
-    return "Can ride 10 chariots at a time naturally!"
+    return "Can ride 10 chariots at a time Naturally!"
 end
 
 function name()
-    return "Ability is one of my name"
+    return "One of my Abilities is one of my name"
 end
 
 print('What are your Abilities?')
@@ -13,9 +13,10 @@ print('What is your Name?')
 print(name())
 
 epic = [[
-Do you know, one of your own sons is going to compete with you driven by mother 
+Do you know, one of your own sons is going to compete with you, driven by mother 
 nature making an epic YANAM creating two more new kingdoms.
-One GIVEN to him and the reamaining will be with you and your other sons.
-Great Empire is on the way!]]
+One GIVEN to him and the remaining will be with you and your other sons.
+Good Great Empire is on the way!
+]]
 
 print(epic)
